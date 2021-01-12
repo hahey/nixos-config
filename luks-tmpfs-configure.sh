8f6d4621069ee0d45f4a1c846b400ef12622fe8f
hahey <heynaheyna9@gmail.com>
2021-01-12 18:03:55 +0100

    fix tmpfs and make it more readable for disk configuration

--- luks-tmpfs-configure.sh
+++ luks-tmpfs-configure.sh
@@ -3,44 +3,50 @@
 # Copyright (C) 2020 Heuna Kim <heynaheyna9@gmail.com>
 
 if [[ $# -ne 5 ]]; then
-    echo "USAGE: $(basename "$0") DISK_DEVICE LVM_PARTITION PHYSICAL_VOLUME VOLUME_GROUP BOOT_PARTITION" >&2
+    echo "USAGE: $(basename "$0") DISK_DEVICE_NAME CRYPT_PARTITION_LUKS BOOT_PARTITION PHYSICAL_VOLUME VOLUME_GROUP " >&2
     exit 1
 fi
 
+DISK_DEVICE="/dev/$1"
+CRYPT_PARTITION_LUKS="/dev/$2"
+BOOT_PARTITION="/dev/$3"
+PHYSICAL_VOLUME="$4"
+VOLUME_GROUP="$5"
+
 # partition
-parted /dev/$1 -- mklabel gpt
-parted /dev/$1 -- mkpart primary 512MiB 300GiB # set aside a remaining space for a future resize
-parted /dev/$1 -- mkpart ESP fat32 1MiB 512MiB # let the boot partition not a part of LUKS
-parted /dev/$1 -- set 3 esp on
+parted "$DISK_DEVICE" -- mklabel gpt
+parted "$DISK_DEVICE" -- mkpart primary 512MiB 300GiB # set aside a remaining space for a future resize
+parted "$DISK_DEVICE" -- mkpart ESP fat32 1MiB 512MiB # let the boot partition not a part of LUKS
+parted "$DISK_DEVICE" -- set 2 esp on
 
 # create LVM on LUKS
-cryptsetup luksFormat /dev/$2
-cryptsetup open /dev/$2 $3
-pvcreate /dev/mapper/$3
-vgcreate $4 /dev/mapper/$3
-lvcreate -L 14G $4-lv01_swap -n swap
-lvcreate -L 32G $4-lv02_root -n root
-lvcreate -L 10G $4-lv03_var -n var
-lvcreate -L 2G $4-lv04_tmp -n tmp
-lvcreate -L 150G $4-lv05_home -n home # set aside a remaining space for a future resize
-#lvcreate -l 100%Free $4-lv05_home -n home
+cryptsetup luksFormat "$CRYPT_PARTITION_LUKS"
+cryptsetup luks open "$CRYPT_PARTITION_LUKS" "$PHYSICAL_VOLUME"
+pvcreate /dev/mapper/"$PHYSICAL_VOLUME"
+vgcreate "$VOLUME_GROUP" /dev/mapper/"$PHYSICAL_VOLUME"
+lvcreate -L 14G "$VOLUME_GROUP" -n lv01_swap
+lvcreate -L 32G "$VOLUME_GROUP" -n lv02_root
+lvcreate -L 10G "$VOLUME_GROUP" -n lv03_var
+lvcreate -L 150G "$VOLUME_GROUP" -n lv04_home # set aside a remaining space for a future resize
+#lvcreate -l 100%Free "$VOLUME_GROUP" -n lv05_home
 
 # Format
-mkfs.ext4 /dev/$4/root
-mkfs.ext4 /dev/$4/var
-mkfs.ext4 /dev/$4/home
-mkfs.ext4 /dev/$4/tmp
-mkswap /dev/$4/swap
-mkfs.fat -F32 /dev/$5
+mkfs.ext4 /dev/"$VOLUME_GROUP"/root
+mkfs.ext4 /dev/"$VOLUME_GROUP"/var
+mkfs.ext4 /dev/"$VOLUME_GROUP"/home
+mkswap /dev/"$VOLUME_GROUP"/swap
+swapon /dev/"$VOLUME_GROUP"/swap
+mkfs.fat -F32 "$BOOT_PARTITION"
 
 # mount
-mkdir /mnt/{boot,var,home,tmp}
-mount /dev/$4/root /mnt
-mount /dev/$4/home /mnt/home
-mount /dev/$5 /mnt/boot
+mount /dev/"$VOLUME_GROUP"/root /mnt
+mkdir /mnt/{home,boot,var,tmp}
 
-mount -o rw,nosuid,nodev /dev/$4/var /mnt/var
-mount -o rw,nosuid,nodev,relatime /dev/$4/tmp /mnt/tmp
+mount /dev/"$VOLUME_GROUP"/home /mnt/home
+mount "$BOOT_PARTITION" /mnt/boot
+
+mount -o rw,nosuid,nodev /dev/"$VOLUME_GROUP"/var /mnt/var
+mount -o rw,nosuid,nodev,relatime,size=6G,mode=1777 TMPFS -t tmpfs /mnt/tmp
 
 #install
 nixos-generate-config --root /mnt
